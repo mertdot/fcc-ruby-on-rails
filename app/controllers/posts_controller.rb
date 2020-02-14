20968c9b37947f47eade7094b4f9b13114fc683d
mertdot <muharremmertozturk@gmail.com>
2020-02-14 19:07:37 +0300

    feb-14

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Posts.all.order("created_at DESC") 
+    @posts = Post.all.order("created_at DESC")
     end
 
   def new
@@ -23,6 +23,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+
+  end
 
   private
 
